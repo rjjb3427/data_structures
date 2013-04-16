fc2dc7d08f22e6ff91ca92cb790d628177ee9576
Eric Wright <dev@ericwright.me>
2013-04-16 16:26:31 -0400

    Flesh out priority queue.

--- labs/lab_3/main.rb
+++ labs/lab_3/main.rb
@@ -1,5 +1,5 @@
 require 'pry'
-
+require 'algorithms'
 
 @printer_num = 5
 
@@ -11,14 +11,16 @@ require 'pry'
 @printer_failure = 6
 # Chances a job will be added.
 @job_percentage = 62
-@random_generator = Random.new
+
+# Global for the sake of brevity.
+$random_generator = Random.new
 
 class Printer
   @@id = -1
   def initialize(status: :idle)
     @id = @@id += 1
     @status = status
-    @repair_time = 0
+    @busy_ticks = 0
     @breakages = 0
   end
   attr_accessor :status, :id, :repair_time, :job
@@ -33,52 +35,75 @@ class Printer
   end
 
   def repair
-    if @repair_time == 0
+    if @busy_ticks == 0
       @status = :idle
       @breakages += 1
     else
-      @repair_time -= 1
+      @busy_ticks -= 1
     end
+  end
+
+  def job=(job)
+    @job = job
+    @status = @job.nil? ? :idle : :printing
   end
 end
 
 class Job
   @@id = -1
-  def initialize(priority: 0, progress: 0)
+  def initialize(priority: $random_generator.rand(10..100), progress: 0)
     @id = @@id += 1
     @priority = priority
   end
   attr_accessor :id, :priority, :progress
+
+  def self.count
+    @@id
+  end
 end
 
 def simulate_printers
+  queue = Containers::PriorityQueue.new
+
   printers = []
   5.times { |i| printers << Printer.new }
 
   # It's like real life printers.
   @day_ticks.times do |i|
+    # Printer status.
     printers.each do |printer|
       if printer.status == :broken
         printer.repair
-      elsif printer.status != :printing && @random_generator.rand(1..100) <= @printer_failure
+      elsif printer.status == :idle && $random_generator.rand(1..100) <= @printer_failure
         printer.status = :broken
-        printer.repair_time = @repair_ticks
+        printer.busy_ticks = @repair_ticks
+      elsif printer.status == :idle
+        printer.job ||= queue.pop
       end
 
       puts printer.pretty_status
     end
 
-    if @random_generator.rand(1..100) <= @job_percentage
-     puts "Job added"
+    # Job creation.
+    if $random_generator.rand(1..100) <= @job_percentage
+     new_job = Job.new
+     queue.push(new_job, new_job.priority)
+
+     puts "Job ##{new_job.id} created, priority #{new_job.priority}"
     end
+
+    # Job queuing.
+    puts "#{queue.size} job(s) on the queue."
     puts "- Tick #{i}"
-        # binding.pry
+        binding.pry
   end
 
   # Time to wrap up the stats for the simulation
   printers.each do |printer|
     puts printer.status_report
   end
+
+  puts "#{queue.size} job(s) created."
 end
 
 simulate_printers
